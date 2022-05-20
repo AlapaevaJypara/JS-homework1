6b712cea72b441ff60def942b32dd8c927a35ded
Jypara Alapaeva <jypara.alapaeva.kk@gmail.com>
2022-05-20 14:49:37 +0600

    add homework1 JS

---
+++ js/script1.js
@@ -0,0 +1,37 @@
+//задание1
+// let userName;
+// userName = "Ivan";
+// document.write(userName);
+
+//Задание 2
+// let userName;
+// userName = "Ivan";
+// document.write(userName);
+// userName = "Pete";
+// document.write(userName);
+
+//Задание3
+// let userName = prompt("What is your name?");
+// let userAge = prompt("How old are you?");
+// let userEmail = prompt("your email?");
+// document.write(
+//   `<h1>Your name is:${userName}.<br> You are ${userAge}  years old <br> Your Email: ${userEmail}</h1>`
+// );
+//Задание4
+
+let souvenir = Number(prompt("Сколько штук сувенира?"));
+let trinket = Number(prompt("Сколько  штук безделушек ?"));
+let souvenirWeight = 75;
+let trinketWeight = 112;
+let result = souvenir * souvenirWeight + trinket * trinketWeight;
+document.write(`<h1>Общий вес посылки составляет: ${result}</h1>`);
+
+//задание5
+// let a = +prompt("Введите любое целое число?");
+// let b = +prompt("Введите второе целое число!");
+// document.write(a + b);
+// document.write(a - b);
+// document.write(a / b);
+// document.write(a + b);
+// document.write(a % b);
+// document.write(a ** b);
